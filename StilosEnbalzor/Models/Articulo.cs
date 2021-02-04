68af139afea658db0c33c45936e1015006b4fe75
chent <chente31209@hotmail.com>
2021-02-04 17:02:17 -0600

    Plantilla De Factura

---
+++ StilosEnbalzor/Models/Articulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StilosEnbalzor.Models
+{
+    public class Articulo
+    {
+        public string articulo {get;set;}
+        public string Nombre {get;set;}
+        public string Um {get;set;}
+        public string Unidades {get;set;}
+        public string Precio {get;set;}
+        public string Decuento {get;set;}
+        public string Importe {get;set;}
+    }
+}
