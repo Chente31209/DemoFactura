178ded15ed3a2767c7ad3ed9b31a6a6752ef565a
josermac <joser_isi@hotmail.com>
2021-02-11 16:57:43 -0600

    add sample nested components

---
+++ StilosEnbalzor/Models/Comprobante.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StilosEnbalzor.Models
+{
+    public class Comprobante
+    {
+        public string Folio {get;set;}
+        public string Cliente {get;set;}
+        public string Fecha {get;set;}
+        public string UsoFDCI{get;set;}
+        public string DomiciloFiscal {get;set;}
+        public string FormaDeCobro{get;set;}
+        public string Moneda{get;set;}
+        public string TipoDeComprobante{get;set;}
+        
+       
+    }
+}
